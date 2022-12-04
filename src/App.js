c8d5573c3be2761612aafb84c126e346694f26a7
aylin güneş <aylngns94@gmail.com>
2022-12-04 16:42:21 +0100

    firs react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <h1> Logo </h1>
+    <ul>
+      <li>main page</li>
+      <li>projects</li>
+      <li>about us</li>
+      <li>contact</li>
+    </ul>
+    </nav>
+    <section className='backimage'>
+  
+  </section>
+  </>
+
+  );
+}
+
+export default App;
